51741a70c38563f76fa86d39110a7812c88d9012
Ajit Nath <nath.ajit01@gmail.com>
2018-09-16 04:47:17 +0800

    BFS

--- TreeTraversal.hs
+++ TreeTraversal.hs
@@ -6,12 +6,23 @@ data Tree a = Empty | Node a (Tree a) (Tree a) deriving (Show)
 
 dfTraversal :: Tree a -> [a]
 
+-- Depth first traversal
+
 dfTraversal Empty                    = []
 dfTraversal (Node root left right)   = root : (dfTraversal left) ++ (dfTraversal right)
 
+-- Breadth first traversal
 
 bfTraversal tree = tbf [tree]
     where
-        -- pass
+        tbf []                              = []
+        tbf xs                              = NodeValue xs ++ (tbf $ concatmap LeftRightNodes xs)
+        NodeValue (a _ _ ) = a
+        LeftRightNodes (Node _ Empty Empty) = []
+        LeftRightNodes (Node _ Empty b)     = [b]
+        LeftRightNodes (Node _ a Empty)     = [a]
+        LeftRightNodes (Node a b)           = [a, b]
+
+
 
 
